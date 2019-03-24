4874e2346a030e15b56002af9508f7c619375b7c
User <User@DESKTOP-FPUSV2D>
2019-03-24 22:07:40 +0900

    Implement Max Month

--- src/honux/Calendar/Calendar.java
+++ src/honux/Calendar/Calendar.java
@@ -1,5 +1,7 @@
 package honux.Calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	public static void main (String[] args) {
 		System.out.println(" 일  월    화   수  목   금  토");
@@ -8,5 +10,13 @@ public class Calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		int i;
+		Scanner sc = new Scanner(System.in);
+		i = sc.nextInt();
+
+		int mon[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		System.out.print(mon[i - 1]);
+		sc.close();
+		//배열 생성해 할당하면(초기화) 배열의 주소가 들어가게 된다.
 	}
 }
