c55bc4b2a5505e9aef7608d6c12a660e089fee8d
User <User@DESKTOP-FPUSV2D>
2019-03-24 22:22:22 +0900

    Update Using class method

--- src/honux/Calendar/Calendar.java
+++ src/honux/Calendar/Calendar.java
@@ -1,22 +1,30 @@
 package honux.Calendar;
-
 import java.util.Scanner;
-
 public class Calendar {
-	public static void main (String[] args) {
+	
+	public static final int mon[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public int month(int i) {
+		return mon[i - 1];
+		
+	}
+	
+	public void PrintSampleCalendar(){
 		System.out.println(" 일  월    화   수  목   금  토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		int i;
+	}
+	
+	public static void main (String[] args) {
+		
+		
+		Calendar cal = new Calendar();
 		Scanner sc = new Scanner(System.in);
-		i = sc.nextInt();
+		int i = sc.nextInt();
 
-		int mon[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		System.out.print(mon[i - 1]);
+		System.out.print(cal.month(i));
 		sc.close();
-		//배열 생성해 할당하면(초기화) 배열의 주소가 들어가게 된다.
 	}
 }
